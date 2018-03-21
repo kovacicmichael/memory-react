05b116229095ed4f1e7b2a5a2c5ed6b3e5a709c7
Michael Kovacic <michael.j.kovacic@gmail.com>
2018-03-21 15:53:03 -0600

    set up added

--- src/App.js
+++ src/App.js
@@ -5,15 +5,32 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+    <div>
+      <nav class="navbar">
+        
+            <ul>
+              <li class = "brand"> <a href="#">Memory Game</a></li>
+              <li class = "status">Click an image to begin!</li>
+              <li class = "score">Score: 0 | Top Score: 0</li>
+            </ul>
+         
+      </nav>
+
+      <header class = "header">
+        <h1>Memory Game!</h1>
+        <h2>Click on an image to earn points, but don't click on any more than once!</h2>
+      </header>
+      <main class = "container">
+
+
+      </main>
+
+      <footer class = "footer">
+          <div class = "bottom">
+          Clicky Game! 
+          </div>
+      </footer>
+    </div>
     );
   }
 }
